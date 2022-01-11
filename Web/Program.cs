8359132a77adf481eeaf179a6a1b9524ebf39631
A Sharp <a.sharp.fus@gmail.com>
2022-01-11 13:56:17 +0200

    azure check

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseRouting();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.Run();
